34c37f91242a4a8400cf285073f080be9a6bb878
matt22881 <47930310+matt22881@users.noreply.github.com>
2020-08-13 00:42:57 -0400

    finish third task

--- index.js
+++ index.js
@@ -40,11 +40,16 @@ To save you from having to count the items above, you can assume that length of 
 
 i.e. is31Flavors(originalFlavors) will return TRUE.*/
 
-function is31Flavors(/*code here*/){
+var emptyArray = []
 
-    /*code here*/
+function is31Flavors(arr1){
 
-}
+    if (arr1.length === 31) return true
+    else return false
+    }
+
+    // console.log(is31Flavors(originalFlavors))
+    // console.log(is31Flavors(emptyArray))
 
 /* Task 2: Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
 
@@ -57,12 +62,14 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
+function addFlavor(newFlavor, arr2){
 
-    /*code here*/
+    arr2.unshift(newFlavor)
+    console.log(arr2)
 
 }
 
+addFlavor('Rainbow Sherbert', originalFlavors)
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
@@ -74,11 +81,14 @@ Your function should remove a flavor from the end of the array and console.log t
 
 For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]*/ 
 
-function removeLastFlavor(/*code here*/){
+function removeLastFlavor(arr3){
 
-    /*code here*/
+    arr3.pop()
+    console.log(arr3)
 
 }
+
+removeLastFlavor(originalFlavors)
 
 /* Task 4: Write a function that returns a flavor at a given index in the array.
 
@@ -89,9 +99,9 @@ Your function should accept:
 
 For example, getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully. */
 
-function getFlavorByIndex(/*code here*/){
+function getFlavorByIndex(arr4, i){
 
-    /*code here*/
+    
 
 }
 
